e7677b65fe139af626229a77cd374a6bcd15beb8
yedilsan <yedilsanatovv@gmail.com>
2023-05-25 02:28:32 +0600

    change

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 
 import SignIn from './pages/Authentication/SignIn';
 //import SignUp from './pages/Authentication/SignUp';
-import Chart from './pages/Chart';
-import FormElements from './pages/Form/FormElements';
-import FormLayout from './pages/Form/FormLayout';
 import AacCategory from './pages/Form/Category';
 import AacPhraseBank from './pages/Form/Phrasebank';
 import AacPhrases from './pages/Form/Phrases';
@@ -43,8 +40,6 @@ function App() {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/forms/form-elements" element={<FormElements />} />
-        <Route path="/forms/form-layout" element={<FormLayout />} />
         <Route path="/forms/Category" element={<AacCategory />} />
         <Route path="/forms/PhraseBank" element={<AacPhraseBank />} />
         <Route path="/forms/Phrases" element={<AacPhrases />} />
@@ -58,7 +53,6 @@ function App() {
         <Route path="/tables" element={<Tables />} />
         <Route path="/users" element={<Users />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/chart" element={<Chart />} />
         <Route path="/ui/alerts" element={<Alerts />} />
         <Route path="/ui/buttons" element={<Buttons />} />
       </Routes>
